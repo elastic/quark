bb91943e3d50a8405b2d1b5525f7084ad2d99371
Christiano Haesbaert <haesbaert@haesbaert.org>
2024-02-03 18:33:53 +0100

    Use linux kernel types
    It's time to admit the linux kernel types are more ergonomical than the
    standard (uint32_t) and BSD (u_int32_t). Linux tools also use a similar scheme
    but they insist on uint64_t for u64.

--- compat.h
+++ compat.h
@@ -1,13 +1,32 @@
 #ifndef _COMPAT_H_
 #define _COMPAT_H_
 
+/* Linux specific */
+#include <linux/types.h>
+
+/* Sys */
 #include <sys/types.h>
 
+/* Standard */
 #include <stdint.h>
 
 /*
  * General compat
  */
+/* uint64_t is historically defined as unsigned long on LONG architectures, not
+ * unsigned long long, meaning we can't always use %llu for printing on 32 and
+ * 64bit. We use __u64 which is saner.
+ */
+typedef __u64		u64;
+typedef __s64		s64;
+typedef __u32		u32;
+typedef __s32		s32;
+typedef __u16		u16;
+typedef __s16		s16;
+typedef __u8		u8;
+typedef __s8		s8;
+typedef uintptr_t	__uintptr_t;	/* for freebsd_tree.h */
+
 #ifndef __aligned
 #define __aligned(x)	__attribute__((aligned(x)))
 #endif
@@ -28,13 +47,12 @@
 #define min(_a, _b)	((_a) < (_b) ? (_a) : (_b))
 #endif	/* min */
 
-#include "freebsd_queue.h"
-typedef uintptr_t __uintptr_t;
-#include "freebsd_tree.h"
-
 /*
  * BSD compat
  */
+#include "freebsd_queue.h"
+#include "freebsd_tree.h"
+
 size_t		strlcat(char *, const char *, size_t);
 size_t		strlcpy(char *, const char *, size_t);
 long long	strtonum(const char *, long long, long long, const char **);
--- quark.h
+++ quark.h
@@ -15,43 +15,43 @@
  * Perf related declarations
  */
 struct perf_sample_id {
-	__u32 pid;
-	__u32 tid;
-	__u64 time;	/* see raw_event_tree_insert_nocol() */
-	__u64 stream_id;
-	__u32 cpu;
-	__u32 cpu_unused;
+	u32	pid;
+	u32	tid;
+	u64	time;		/* see raw_event_tree_insert_nocol() */
+	u64	stream_id;
+	u32	cpu;
+	u32	cpu_unused;
 };
 
 struct perf_record_fork {
 	struct perf_event_header	header;
-	__u32				pid;
-	__u32				ppid;
-	__u32				tid;
-	__u32				ptid;
-	__u64				time;
+	u32				pid;
+	u32				ppid;
+	u32				tid;
+	u32				ptid;
+	u64				time;
 	struct perf_sample_id		sample_id;
 };
 
 struct perf_record_exit {
 	struct perf_event_header	header;
-	__u32				pid;
-	__u32				ppid;
-	__u32				tid;
-	__u32				ptid;
-	__u64				time;
+	u32				pid;
+	u32				ppid;
+	u32				tid;
+	u32				ptid;
+	u64				time;
 	struct perf_sample_id		sample_id;
 };
 
 struct perf_data_loc {
-	__u16	offset;
-	__u16	size;
+	u16	offset;
+	u16	size;
 };
 
 struct perf_record_sample {
 	struct perf_event_header	header;
 	struct perf_sample_id		sample_id;
-	__u32				size;
+	u32				size;
 	char				data[];
 };
 
@@ -69,8 +69,8 @@ struct perf_mmap {
 	size_t				 mapped_size;
 	size_t				 data_mask;
 	uint8_t				*data_start;
-	__u64				 data_tmp_tail;
-	__u8				 wrapped_event_buf[4096] __aligned(8);
+	u64				 data_tmp_tail;
+	u8				 wrapped_event_buf[4096] __aligned(8);
 };
 
 /* More local perf related declarations */
