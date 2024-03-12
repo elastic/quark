cdc095e2dcdd45a6cb68ae7803393edcbb891e49
Christiano Haesbaert <haesbaert@elastic.co>
2024-03-13 00:32:46 +0100

    Improve Go bindings, thanks to Dan!
    This addresses the points Dan brought up during the review, which was excellent
    and educational, many thanks again!

--- go/quark.go
+++ go/quark.go
@@ -1,4 +1,5 @@
 package main
+
 /*
    #cgo CFLAGS: -I../
    #cgo LDFLAGS: ./../libquark.a ./../libbpf/src/libbpf.a -lelf -lz
@@ -11,48 +12,42 @@ import "C"
 import (
 	"errors"
 	"fmt"
+
 	"golang.org/x/sys/unix"
 )
 
 type QuarkProcEvent struct {
 	cap_inheritable uint64
-	cap_permitted uint64
-	cap_effective uint64
-	cap_bset uint64
-	cap_ambient uint64
-	time_boot uint64
-	time_event uint64
-	time_start uint64
-	ppid uint32
-	uid uint32
-	gid uint32
-	suid uint32
-	sgid uint32
-	euid uint32
-	egid uint32
+	cap_permitted   uint64
+	cap_effective   uint64
+	cap_bset        uint64
+	cap_ambient     uint64
+	time_boot       uint64
+	time_event      uint64
+	time_start      uint64
+	ppid            uint32
+	uid             uint32
+	gid             uint32
+	suid            uint32
+	sgid            uint32
+	euid            uint32
+	egid            uint32
 }
 
 type QuarkEvent struct {
-	/* Always present */
-	pid uint32
-	/* QUARK_EV_PROC */
-	proc *QuarkProcEvent
-	/* QUARK_EV_EXIT */
-	exit_code *int32
-	/* QUARK_EV_COMM */
-	comm string
-	/* QUARK_EV_FILENAME */
-	filename string
-	/* QUARK_EV_CMDLINE */
-	cmdline string
-	/* QUARK_EV_CWD */
-	cwd string
+	pid       uint32          // Always present
+	proc      *QuarkProcEvent // QUARK_EV_PROC
+	exit_code *int32          // QUARK_EV_EXIT
+	comm      string          // QUARK_EV_COMM
+	filename  string          // QUARK_EV_FILENAME
+	cmdline   string          // QUARK_EV_CMDLINE
+	cwd       string          // QUARK_EV_CWD
 }
 
 type QuarkQueue struct {
-	qqc C.struct_quark_queue
+	qqc  C.struct_quark_queue
 	cevs []C.struct_quark_event
-	fds []unix.PollFd
+	fds  []unix.PollFd
 }
 
 var ErrUndefined = errors.New("Undefined")
@@ -79,26 +74,26 @@ func QuarkClose() {
 }
 
 func QuarkQueueOpen(slots int) (*QuarkQueue, error) {
-	qq := QuarkQueue{}
+	var qq QuarkQueue
 
 	r, err := C.quark_queue_open(&qq.qqc, 0)
 	if r == -1 {
 		return nil, wrapErrno(err)
 	}
-	for ; slots > 0; slots-- {
-		qq.cevs = append(qq.cevs, C.struct_quark_event{})
-	}
+	qq.cevs = make([]C.struct_quark_event, slots)
+
 	var fdsa [1024]C.int
 	nfds, err := C.quark_queue_get_fds(&qq.qqc, &fdsa[0], C.int(len(fdsa)))
 	if nfds == -1 {
 		return nil, wrapErrno(err)
 	}
-	for i := 0; i < int(nfds); i++ {
-		pfd := unix.PollFd{
-			Fd: int32(fdsa[i]),
+
+	qq.fds = make([]unix.PollFd, nfds)
+	for i := range qq.fds {
+		qq.fds[i] = unix.PollFd{
+			Fd:     int32(fdsa[i]),
 			Events: unix.POLLIN | unix.POLLHUP,
 		}
-		qq.fds = append(qq.fds, pfd)
 	}
 
 	return &qq, nil
@@ -109,15 +104,14 @@ func (qq *QuarkQueue) Close() {
 }
 
 func (qq *QuarkQueue) GetEvents() ([]QuarkEvent, error) {
-	nc, err := C.quark_queue_get_events(&qq.qqc, &qq.cevs[0],
+	n, err := C.quark_queue_get_events(&qq.qqc, &qq.cevs[0],
 		C.int(len(qq.cevs)))
-	n := int(nc)
 	if n == -1 {
 		return nil, wrapErrno(err)
 	}
 
 	qqevs := make([]QuarkEvent, n)
-	for i := 0; i < n; i++ {
+	for i := range qqevs {
 		qev, err := eventToGo(&qq.cevs[i])
 		if err != nil {
 			panic(err)
@@ -137,39 +131,39 @@ func eventToGo(cev *C.struct_quark_event) (QuarkEvent, error) {
 	var qev QuarkEvent
 
 	qev.pid = uint32(cev.pid)
-	if cev.flags & C.QUARK_EV_PROC != 0 {
+	if cev.flags&C.QUARK_EV_PROC != 0 {
 		qev.proc = &QuarkProcEvent{
 			cap_inheritable: uint64(cev.proc_cap_inheritable),
-			cap_permitted: uint64(cev.proc_cap_permitted),
-			cap_effective: uint64(cev.proc_cap_effective),
-			cap_bset: uint64(cev.proc_cap_bset),
-			cap_ambient: uint64(cev.proc_cap_ambient),
-			time_boot: uint64(cev.proc_time_boot),
-			time_event: uint64(cev.proc_time_event),
-			time_start: uint64(cev.proc_time_start),
-			ppid: uint32(cev.proc_ppid),
-			uid: uint32(cev.proc_uid),
-			gid: uint32(cev.proc_gid),
-			suid: uint32(cev.proc_suid),
-			sgid: uint32(cev.proc_sgid),
-			euid: uint32(cev.proc_euid),
-			egid: uint32(cev.proc_egid),
+			cap_permitted:   uint64(cev.proc_cap_permitted),
+			cap_effective:   uint64(cev.proc_cap_effective),
+			cap_bset:        uint64(cev.proc_cap_bset),
+			cap_ambient:     uint64(cev.proc_cap_ambient),
+			time_boot:       uint64(cev.proc_time_boot),
+			time_event:      uint64(cev.proc_time_event),
+			time_start:      uint64(cev.proc_time_start),
+			ppid:            uint32(cev.proc_ppid),
+			uid:             uint32(cev.proc_uid),
+			gid:             uint32(cev.proc_gid),
+			suid:            uint32(cev.proc_suid),
+			sgid:            uint32(cev.proc_sgid),
+			euid:            uint32(cev.proc_euid),
+			egid:            uint32(cev.proc_egid),
 		}
 	}
-	if cev.flags & C.QUARK_EV_EXIT != 0 {
+	if cev.flags&C.QUARK_EV_EXIT != 0 {
 		exit_code := int32(cev.exit_code)
 		qev.exit_code = &exit_code
 	}
-	if cev.flags & C.QUARK_EV_COMM != 0 {
+	if cev.flags&C.QUARK_EV_COMM != 0 {
 		qev.comm = C.GoString(&cev.comm[0])
 	}
-	if cev.flags & C.QUARK_EV_FILENAME != 0 {
+	if cev.flags&C.QUARK_EV_FILENAME != 0 {
 		qev.filename = C.GoString(&cev.filename[0])
 	}
-	if cev.flags & C.QUARK_EV_CMDLINE != 0 {
+	if cev.flags&C.QUARK_EV_CMDLINE != 0 {
 		qev.cmdline = C.GoString(&cev.cmdline[0])
 	}
-	if cev.flags & C.QUARK_EV_CWD != 0 {
+	if cev.flags&C.QUARK_EV_CWD != 0 {
 		qev.cwd = C.GoString(&cev.cwd[0])
 	}
 
