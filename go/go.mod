e9ac3c658b916875b37cc989ec3995051c75b0b7
fearful-symmetry <alexpkristiansen@icloud.com>
2024-08-07 08:13:49 -0700

    code cleanup, add tests

--- go/go.mod
+++ go/go.mod
@@ -3,3 +3,10 @@ module github.com/elastic/quark/go
 go 1.21.1
 
 require golang.org/x/sys v0.18.0
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ go/quark_test.go
@@ -0,0 +1,37 @@
+package quark
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestQuarkLookup(t *testing.T) {
+	queue, err := OpenQueue(DefaultQueueAttr(), 64)
+	require.NoError(t, err)
+
+	defer queue.Close()
+
+	fetchPid := uint32(1)
+	pid1, ok := queue.Lookup(int(fetchPid))
+	require.True(t, ok)
+
+	require.Equal(t, fetchPid, pid1.Pid)
+	require.NotEmpty(t, pid1.Comm)
+	require.NotEmpty(t, pid1.Cwd)
+}
+
+func TestQuarkGetEvents(t *testing.T) {
+	queue, err := OpenQueue(DefaultQueueAttr(), 64)
+	require.NoError(t, err)
+
+	defer queue.Close()
+
+	qevs, err := queue.GetEvents()
+	require.NoError(t, err)
+
+	for _, qev := range qevs {
+		require.NotEmpty(t, qev.Comm)
+		require.NotEmpty(t, qev.Cwd)
+	}
+}
