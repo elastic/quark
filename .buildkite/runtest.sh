9676ca6bc85a364a79a1a4c00f9a5fe749a3872d
Christiano Haesbaert <haesbaert@elastic.co>
2024-11-07 15:52:36 +0100

    yadayada

--- .buildkite/runtest.sh
+++ .buildkite/runtest.sh
@@ -12,8 +12,10 @@ if [ -z "${BUILDKITE}" ]; then
 	exit 1
 fi
 
+download initramfs.gz .
 download quark-test .
 chmod +x quark-test
 
 sudo ./quark-test
+./krun.sh initramfs.gz kernel-images/linux-4.18.0-553.el8_10.x86_64 quark-test
 exit $?
