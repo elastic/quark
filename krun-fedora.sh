6d7377e173d55f1d820f2e3243d3ced8a2d72569
Christiano Haesbaert <haesbaert@elastic.co>
2025-04-08 08:02:47 +0200

    Test latest Fedora kernels from fc32-41
    This improves the CI by running quark-test on all fedora versions.
    
    krun-fedora.sh downloads the latest kernel rpm of the specified version, unpacks
    and runs krun.sh on it. When Fedora updates its rpms, we end up getting the
    updated version.
    
    This also enables nested virtualization on the CI so we can run things on qemu
    super fast, <1min as opposed to 5-10min on the worst case, the most important
    bit is that it fixes some flaky tests that would time out.
    
    Prompted by a question from @andrewkroh
    
    Many thanks to @mjwolf for pointing me to the allowed GCP options in buildkite.

---
+++ krun-fedora.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT=${0##*/}
+
+function usage
+{
+	echo "usage: $SCRIPT initramfs.gz FEDORAVERSION command" 1>&2
+	exit 1
+}
+
+if [ $# -lt 3 ]; then
+	usage
+fi
+
+INITRAMFS="$1"
+FEDORAVER="$2"
+shift 2
+
+case $FEDORAVER in
+2?|3?)	URL="https://archives.fedoraproject.org/pub/archive/fedora/linux/updates/$FEDORAVER/Everything/x86_64/Packages/k";;
+4?)	URL="http://ftp.fau.de/fedora/linux/updates/$FEDORAVER/Everything/x86_64/Packages/k";;
+*)	echo bad version "$FEDORAVER" 1>&2
+esac
+
+TMPDIR=$(mktemp -d "/tmp/$SCRIPT.XXXXXXXXXX")
+trap 'rm -rf "$TMPDIR"' EXIT
+
+RPMURL=$(lynx -dump -listonly "$URL"|grep kernel-core)
+RPMURL=${RPMURL##* }
+RPM=$(basename "$RPMURL")
+VMLINUZ=${RPM##kernel-core-}
+VMLINUZ=${VMLINUZ%%.rpm}
+VMLINUZ=$TMPDIR/lib/modules/$VMLINUZ/vmlinuz
+
+# echo URL $URL
+# echo RPMURL $RPMURL
+# echo RPM $RPM
+# echo VMLINUZ $VMLINUZ
+
+cd "$TMPDIR"
+curl -s "$RPMURL" | rpm2cpio - | cpio -idm
+cd -
+
+./krun.sh "$INITRAMFS" "$VMLINUZ" "$@"
