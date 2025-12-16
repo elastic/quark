4e8a06a83280a0b3836691389b6edbcaa5ca2641
Christiano Haesbaert <haesbaert@elastic.co>
2025-12-16 08:50:56 +0100

    Zap QUARK_F_CONTAINER
    This is a leftover from when strings were not allocated and just copied around,
    no need.
    
    Issue #216

--- quark.h
+++ quark.h
@@ -573,7 +573,6 @@ struct quark_process {
 #define QUARK_F_FILENAME	(1 << 3)
 #define QUARK_F_CMDLINE		(1 << 4)
 #define QUARK_F_CWD		(1 << 5)
-#define QUARK_F_CONTAINER	(1 << 7)
 	u64	 flags;
 
 	u64	 poison_tag;	/* valid if != 0 */
