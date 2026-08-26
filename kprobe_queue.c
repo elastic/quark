8f1fd621267a08c8677e2c78ef28289fb30c9bf8
Christos Kalkanis <christos.kalkanis@elastic.co>
2026-08-26 16:00:24 -0400

    Fix indent

--- kprobe_queue.c
+++ kprobe_queue.c
@@ -1542,7 +1542,7 @@ kprobe_queue_update_stats(struct quark_queue *qq)
 {
 	struct kprobe_queue 		*kqq = qq->queue_be;
 	struct perf_group_leader	*pgl;
-	u64				stalls = 0;
+	u64				 stalls = 0;
 
 	TAILQ_FOREACH(pgl, &kqq->perf_group_leaders, entry)
 		stalls += pgl->mmap.stalled;
